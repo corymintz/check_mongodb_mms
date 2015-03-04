8de496cbcfb179ffff1d8f2c3007f0642a1a450a
corymintz <cory.mintz@10gen.com>
2015-03-04 11:45:09 -0500

    skeleton for nagios_config_gen

---
+++ model/hosts.go
@@ -0,0 +1,9 @@
+// Copyright 2015 MongoDB, Inc. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package model
+
+type Host struct {
+	Id string `json:"id"`
+}
---
+++ nagios_config_gen.go
@@ -0,0 +1,8 @@
+// Copyright 2015 MongoDB, Inc. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+}
